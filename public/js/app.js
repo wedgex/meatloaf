a4f9d13b4a7bfe4992e558579f01bc24eda947e0
Hunter Haydel <haydh530@gmail.com>
2013-05-21 11:44:47 -0500

    clean up the map a bit

--- public/js/app.js
+++ public/js/app.js
@@ -2,23 +2,14 @@ $(document).ready(function() {
   $.getJSON('/weltys', function(data) {
     var klass;
 
-    switch(data.status) {
-      case 'ERROR':
-	klass = 'blackwatch';
-        break;
-      case 'MAYBE':
-        klass = 'blackwatch';
-        break;
-      case 'YES':
-        klass = 'green';
-        break;
-      case 'NO':
-        klass = 'red';
-        break;
-      default:
-        klass = 'blackwatch';
-        break;
-    }
+    var statusMap = {
+      'ERROR': 'moving-pictures',
+      'MAYBE': 'blackwatch',
+      'YES': 'green',
+      'NO': 'red'
+    };
+
+    klass = statusMap[data.status];
 
     $('body').addClass(klass);
   });
