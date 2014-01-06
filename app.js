a69440b4c2d72e367547ea594fc973c21e2f113f
Hunter Haydel <haydh530@gmail.com>
2014-01-06 09:23:32 -0600

    meat<space>loaf?!

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ app.get('/', function(req, res) {
 app.get('/weltys', function(req, res) {
  request('http://www.weltysdeli.com/daily.html', function(error, response, body) {
   var status;
-  var meatloaf_melty = /meatloaf melty/i;
-  var meatloaf = /meatloaf/i;
+  var meatloaf_melty = /meat( )?loaf melty/i;
+  var meatloaf = /meat( )?loaf/i;
 
   if (error || response.statusCode != 200) {
     status = 'ERROR';
